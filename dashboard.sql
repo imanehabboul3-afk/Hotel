06c84bc04db668e09e9467564bc9d023ed6894d5
imanehabboul3-afk <imanehabboul3@gmail.com>
2026-08-15 01:19:56 +0000

    Premier commit

--- dashboard.sql
+++ dashboard.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : sam. 15 août 2026 à 02:50
+-- Généré le : sam. 15 août 2026 à 03:16
 -- Version du serveur : 10.4.32-MariaDB
 -- Version de PHP : 8.2.12
 
@@ -37,13 +37,6 @@ CREATE TABLE `messages` (
   `date_envoi` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Déchargement des données de la table `messages`
---
-
-INSERT INTO `messages` (`id`, `nom`, `telephone`, `email`, `sujet`, `message`, `date_envoi`) VALUES
-(1, 'Imane Habboul', '0690760945', 'imanehabboul3@gmail.com', 'réservation', 'une très bonne service', '2026-08-11 12:16:40');
-
 -- --------------------------------------------------------
 
 --
@@ -66,15 +59,6 @@ CREATE TABLE `reservations` (
   `status` varchar(20) DEFAULT 'En attente',
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Déchargement des données de la table `reservations`
---
-
-INSERT INTO `reservations` (`id`, `checkin`, `checkout`, `adults`, `children`, `room_type`, `firstname`, `lastname`, `email`, `phone`, `requests`, `payment`, `status`, `created_at`) VALUES
-(2, '2026-08-15', '2026-08-21', 1, 0, 'royal', 'Imane', 'Habboul', 'imanehabboul3@gmail.com', '+212690760945', 'régime alimentaire  spéciale', 'card', 'Refusée', '2026-08-13 13:29:43'),
-(6, '2026-12-12', '2026-12-16', 1, 0, 'royal', 'Imane', 'Habboul', 'imanehabboul3@gmail.com', '+212690760945', '', 'hotel', 'Acceptée', '2026-08-13 18:51:24'),
-(7, '2026-08-20', '2026-08-19', 2, 0, 'royal', 'Imane', 'Habboul', 'imanehabboul3@gmail.com', '+212690760945', '', 'hotel', 'En attente', '2026-08-13 22:25:12');
 
 --
 -- Index pour les tables déchargées
